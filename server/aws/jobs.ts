ffdfbac9cb923d67a364453ad3e0fb617e047711
max <max@humpherys.dev>
2025-06-18 22:07:42 -0600

    automation to load bucket with champion square images

--- server/aws/jobs.ts
+++ server/aws/jobs.ts
@@ -6,29 +6,27 @@ import { championNames } from '../constants/championNames.ts';
 
 const storeAssets = async () => {
     const version = await getVersion();
-    const champs = ['Jax', 'Zed'];
-
-    // champs.forEach(async (champion) => {
-    //     const img = await getSquareImg(champion, version);
-    //     client.send(
-    //         putObject({
-    //             Bucket: 'loldle',
-    //             Key: `${champion}/${champion}`,
-    //             Body: img,
-    //         })
-    //     );
-    // });
-
-    champs.forEach(async (champion) => {
-        const response = await isInBucket(`${champion}/${champion}`);
-        console.log(response);
+    championNames.forEach(async (champion) => {
+        try {
+            // TODO: Util function that calls getObjectMetaData and handles returns error
+            await client.send(
+                getObjectMetaData({
+                    Bucket: 'loldle',
+                    Key: `${champion}/${champion}`,
+                })
+            );
+        } catch {
+            const img = await getSquareImg(champion, version);
+            client.send(
+                putObject({
+                    Bucket: 'loldle',
+                    Key: `${champion}/${champion}`,
+                    Body: img,
+                    ContentType: 'image/png',
+                })
+            );
+        }
     });
-};
-
-const isInBucket = async (objectKey: string) => {
-    console.log(
-        client.send(getObjectMetaData({ Bucket: 'loldle', Key: objectKey }))
-    );
 };
 
 storeAssets();
