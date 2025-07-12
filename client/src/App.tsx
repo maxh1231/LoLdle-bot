849600620be84a4c38c931f0ea9c1360e9e742da
max <max@humpherys.dev>
2025-07-11 23:49:29 -0600

    client side requests to obtain daily classic champion which is passed to respective component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,44 @@
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router';
 import { Landing, Classic } from './pages';
 
+interface DailyChampion {
+    classic: number;
+    // mode: number,
+    // mode: number,
+    // mode: number,
+}
 function App() {
+    const [dailyChampions, setDailyChampions] = useState<DailyChampion | null>(
+        null
+    );
+
+    // Query local JSON with champion ID and pass Champion to props,
+    // or pass champion ID to respective component and each component
+    // queries local JSON?
+    useEffect(() => {
+        (async () => {
+            // discord sdk may automatically proxy requests to /api
+            const response = await fetch(`/api/champion/classic`);
+            const data = await response.json();
+            setDailyChampions({ classic: data.id });
+        })();
+    }, []);
+
+    console.log(dailyChampions);
     return (
         <main className='min-h-screen'>
             <Routes>
                 <Route path='/' element={<Landing />} />
-                <Route path='/classic' element={<Classic />} />
+                {/* May want to render one page with each modes' component */}
+                {dailyChampions?.classic && (
+                    <Route
+                        path='/classic'
+                        element={
+                            <Classic solutionId={dailyChampions?.classic} />
+                        }
+                    />
+                )}
             </Routes>
         </main>
     );
--- client/src/pages/Classic.tsx
+++ client/src/pages/Classic.tsx
@@ -1,22 +1,19 @@
 import { ClassicGuess, Submit } from '../components';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import type { Champion } from '../types/champion';
+import championData from '../assets/championData.json';
 
-// Development DailyChampion
-const DailyChampion: Champion = {
-    champion_name: 'Alistar',
-    gender: 'M',
-    positions: ['Support'],
-    range_type: ['Melee'],
-    regions: ['Runeterra'],
-    release_date: '2020-02-21',
-    resource: 'Mana',
-    species: ['Minotaur'],
-};
-
-const Classic = () => {
+interface ClassicProps {
+    solutionId: number;
+}
+const Classic: React.FC<ClassicProps> = ({ solutionId }) => {
     const [attempt, setAttempts] = useState<Champion[] | null>(null);
+    const [solution, setSolution] = useState<Champion | undefined>(undefined);
     const colHeader = 'flex justify-center items-center w-17 h-18';
+
+    useEffect(() => {
+        setSolution(championData.find((item) => item.id == solutionId));
+    }, [solutionId]);
     return (
         <section className='flex flex-col m-auto'>
             <Submit setAttempts={setAttempts} />
@@ -30,13 +27,14 @@ const Classic = () => {
                     <h2 className={colHeader}>Range Type</h2>
                     <h2 className={colHeader}>Region(s)</h2>
                     <h2 className={colHeader}>Release Year</h2>
-                    {attempt.map((item, i) => (
-                        <ClassicGuess
-                            key={i}
-                            attempt={item}
-                            solution={DailyChampion}
-                        />
-                    ))}
+                    {solution &&
+                        attempt.map((item, i) => (
+                            <ClassicGuess
+                                key={i}
+                                attempt={item}
+                                solution={solution}
+                            />
+                        ))}
                 </div>
             )}
         </section>
