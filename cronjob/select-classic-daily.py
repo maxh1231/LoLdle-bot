09e78ec28ff5f355b830043739dc9e8b43e93870
max <max@humpherys.dev>
2025-07-11 21:05:16 -0600

    insert selected champion & reset its weight, increment all other champions

--- cronjob/select-classic-daily.py
+++ cronjob/select-classic-daily.py
@@ -19,15 +19,33 @@ def main():
 
     cursor.execute("SELECT SUM(weight) FROM champions")
     sum = int(cursor.fetchone()[0])
-    rand = int(random.randrange(sum, (sum + 1)))
+    rand = int(random.randrange(1, (sum + 1)))
+
 
     pointer = int(0) 
+    championId = None
     for row in champions:
         pointer += row[1]
         if pointer >= rand:
             print(f"Total weight: {sum}, random number: {rand}")
             print(f"pointer {pointer} found: id {row[0]}, weight: {row[1]}")
+            championId = int(row[0])
             break
+
+
+    # TODO: Clean this up, separate into more readable functions, try/catch, etc
+    if championId is not None:
+        # Insert selected daily champion
+        cursor.execute(f"INSERT INTO classic_daily (champion_id) VALUES ({championId})")
+        conn.commit()
+
+        # Reset selected champion's weight
+        cursor.execute(f"UPDATE champions SET weight = 1 WHERE id = {championId}")
+        conn.commit()
+
+        # Increment all other champion's weight
+        cursor.execute(f"UPDATE champions SET weight = weight + 1 where id != {championId}")
+        conn.commit()
 
     cursor.close()
     conn.close()
